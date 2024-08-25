25f8c0b31951a7bd9f23aeb3dd3bc850d6b4d135
Satish Rathod <satishrathod.dev@gmail.com>
2024-08-25 18:53:42 +0530

    finished

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -1,28 +1,30 @@
-import React, { useContext } from 'react'
-import { NotificationContext } from '../context/NotificationContext'
-import { UserContext } from '../context/UserContext'
+import React, { useContext } from "react";
+import { NotificationContext } from "../context/NotificationContext";
+import { UserContext } from "../context/UserContext";
 
 function Notifications() {
-	const { notifications } = useContext(NotificationContext)
-	const { users } = useContext(UserContext)
+  const { notifications } = useContext(NotificationContext);
+  const { users } = useContext(UserContext);
 
-	const getAuthor = (id) => {
-		return users.find((user) => user.id == id)
-	}
+  const getAuthor = (id) => {
+    return users.find((user) => user.id == id);
+  };
 
-	return (
-		<section className='container notifications'>
-			<h2>Notifications</h2>
-			<section>
-					{notifications.map((notification) => (
-						<section key={notification.id}>
-							<strong>{getAuthor(notification.authorId).name}</strong>{' '}
-							{notification.message}
-						</section>
-					))}
-			</section>
-		</section>
-	)
+  return (
+    <section className="container notifications">
+      <h2>Notifications</h2>
+      <section>
+        <div className="notificationsList">
+          {notifications.map((notification) => (
+            <section key={notification.id}>
+              <strong>{getAuthor(notification.authorId).name}</strong>{" "}
+              {notification.message}
+            </section>
+          ))}
+        </div>
+      </section>
+    </section>
+  );
 }
 
-export default Notifications
+export default Notifications;
